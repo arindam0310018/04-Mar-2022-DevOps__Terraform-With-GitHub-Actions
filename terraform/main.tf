ba76426d8bc03ab20c1c56d5fbc4a69ee20b2f4d
Arindam Mitra <mail2arindam2003@yahoo.com>
2022-03-03 15:38:41 +0100

    Create main.tf
    Terraform Main File with RG Deployment

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfpipeline-rg"
+    storage_account_name = "tfpipelinesa"
+    container_name       = "terraform"
+    key                  = "GithubActions/tfyamlgithubv1-0.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.96"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+
+resource "azurerm_resource_group" "rg1" {
+  name     = "AM-RG-Github-1"
+  location = "westeurope"
+}
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "AM-RG-Github-2"
+  location = "westeurope"
+}
